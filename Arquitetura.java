cd6983f138d40b42f18aa3f4699ed3c0f25adef5
kamei <leokamei1@gmail.com>
2024-06-22 20:36:06 -0300

    a

--- Arquitetura.java
+++ Arquitetura.java
@@ -3,17 +3,17 @@ import CPU.SuperEscalar;
 
 public class Arquitetura {
     public static void main(String[] args) {
-         Interface i = new Interface();
-         i.criarInterface();
-        // String[] pathProcessos = { "./processo1.txt", "./processo3.txt" };
-        // SuperEscalar a = new SuperEscalar(2, pathProcessos);
-        // a.rodarCodigo("IMT");
-        // a.rodarCodigo("IMT");
-        // a.rodarCodigo("IMT");
-        // a.rodarCodigo("IMT");
-        // a.rodarCodigo("IMT");
-        // a.rodarCodigo("IMT");
-        // a.rodarCodigo("IMT");
-        // a.rodarCodigo("IMT");
+        //  Interface i = new Interface();
+        //  i.criarInterface();
+        String[] pathProcessos = { "./processo1.txt", "./processo3.txt" };
+        SuperEscalar a = new SuperEscalar(2, pathProcessos);
+        a.rodarCodigo("SMT");
+        a.rodarCodigo("SMT");
+        a.rodarCodigo("SMT");
+        a.rodarCodigo("SMT");
+        a.rodarCodigo("SMT");
+        a.rodarCodigo("SMT");
+        a.rodarCodigo("SMT");
+        a.rodarCodigo("SMT");
     }
 }
